68aceb168c3fe3aca9325d549c6934de1ecab53f
Finn Oriander <101687223+FinnOriander@users.noreply.github.com>
2022-06-14 23:57:57 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -10,10 +10,64 @@ const sounds = [
     "./Drumkit_Sounds/tom.wav"
 ]
 
-
+let recording = false;
+let playing = false;
+let recordSequence = [];
+let speed = 1000;
+let loopInterval = 1000;
+const speedLabel = document.getElementById("speedLabel")
+let interval = setInterval(() => { }, 1000);
 
 play = (i) => {
     sound = new Audio(sounds[i]);
     sound.play();
 }
-
+playSequence = () => {
+    if (!playing)
+        return;
+    recording = false;
+    for (let i = 0; i <= recordSequence.length; i++) {
+        setTimeout(() => {
+            play(recordSequence[i]);
+        }, speed * i);
+    }
+}
+startLoop = () => {
+    playing = true;
+    recording = false;
+    calcIntervalDelay();
+    playSequence();
+}
+endLoop = () => {
+    playing = false;
+    recording = false;
+}
+record = () => {
+    recordSequence = [];
+    recording = true;
+}
+displayLabel = () => {
+    speedLabel.innerHTML = `Delay: ${speed / 1000}s`;
+}
+faster = () => {
+    if (speed == 100)
+        return;
+    speed -= 100;
+    calcIntervalDelay();
+    displayLabel();
+}
+slower = () => {
+    speed += 100;
+    displayLabel();
+    calcIntervalDelay();
+}
+normalSpeed = () => {
+    speed = 1000;
+    displayLabel();
+    calcIntervalDelay();
+}
+calcIntervalDelay = () => {
+    clearInterval(interval);
+    loopInterval = speed * recordSequence.length;
+    interval = setInterval(playSequence, loopInterval);
+}
